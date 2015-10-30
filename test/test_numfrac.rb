cf1f7bcfe7a55f1ccca5fc6ab6d6382c3ffed172
alu0100851236 <alu0100851236@ull.edu.es>
2015-10-30 14:38:24 +0000

    Arreglado archivo de test y añadido gcd.rb

--- test/test_numfrac.rb
+++ test/test_numfrac.rb
@@ -5,28 +5,33 @@ require 'test/unit'
 class TestNumNumFrac < Test::Unit::TestCase
   
   def setup
-    @origen = NumFrac.new(2,6)
-    @unidad = NumFrac.new(3,9)
+    @origen = NumFrac.new(6,4)
+    @unidad = NumFrac.new(2,8)
     
   end
   
-  def test_sum
-    assert_equal("(12,18)", (@origen + @unidad).to_s)
+  def test_equal
+    assert_equal("6/4", @origen.to_s)
     
   end
   
-  def test_rest
-    assert_equal("(0,18)", (@origen - @unidad).to_s)
+  def test_suma
+    assert_equal("56/32", (@origen + @unidad).to_s)
+    
+  end
+  
+  def test_resta
+    assert_equal("40/32", (@origen - @unidad).to_s)
   
   end
   
-  def test_mult
-    assert_equal("(6,54)", (@origen * @unidad).to_s)
+  def test_multi
+    assert_equal("12/32", (@origen * @unidad).to_s)
     
   end
   
   def test_div
-    assert_equal("(18,18)", (@origen / @unidad).to_s)
+    assert_equal("48/8", (@origen / @unidad).to_s)
   end
     
 
